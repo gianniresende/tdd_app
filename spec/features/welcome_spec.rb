84c1309484331da7e031b8aba0fe0557ecba0cbe
gianniresende <gianniresende@gmail.com>
2021-04-30 19:25:04 -0300

    test: should exist link new client at welcome

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 feature "Welcome", type: :feature do
   scenario 'Show the message of welcome' do
-    visit('/')
+    visit(root_path)
     expect(page).to have_content('Welcome') 
+  end
+
+  scenario 'Verify link register client' do 
+    visit(root_path)
+    expect(find('ul li')).to have_link('New Client')
   end
 end
