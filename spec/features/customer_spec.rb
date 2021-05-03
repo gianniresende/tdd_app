3a59a4fd0f6fb12ec0e8e537d0225ee38312d257
gianniresende <gianniresende@gmail.com>
2021-05-02 21:35:58 -0300

    test: verify fields at form new client

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -16,6 +16,19 @@ feature "Customers", type: :feature do
   scenario 'Verify form new client' do 
     visit(customers_path)
     click_on('New Client')
-    expect(page).to have_content('New Client')
+    expect(page).to have_content('Novo Cliente')
+  end
+
+  scenario 'Register new Client' do 
+    customer_name = Faker::Name.name
+    visit(new_customer_path)
+    fill_in "Nome",	with: customer_name
+    fill_in "Email",	with: Faker::Internet.email 
+    fill_in "Telefone",	with: Faker::PhoneNumber.phone_number
+    attach_file "Foto do Perfil", "#{Rails.root}/spec/fixtures/avatar.png" 
+    choose(option: ['S','N'].sample)
+    click_on('Salvar Cliente')
+    expect(page).to have_content('Client registered success')
+    expect(Customer.last.name).to eq(customer_name) 
   end
 end
