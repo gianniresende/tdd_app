11d3e41abf4e36e16c7ee7406bdf9a773202bf7f
gianniresende <gianniresende@gmail.com>
2021-05-03 15:36:56 -0300

    test: verify link_to delete customer in index

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -104,4 +104,36 @@ feature "Customers", type: :feature do
 
     expect(page).to have_content('Mostrando Cliente')
   end
+
+  scenario 'Click link to edit' do  
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S','N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, "/html/body/table/tbody/tr/td[5]/a").click
+
+    expect(page).to have_content('Editando Cliente')
+  end
+
+  scenario 'Click link to delete', js: true do  
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S','N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, "/html/body/table/tbody/tr/td[6]/a").click
+    1.second
+    page.driver.browser.switch_to.alert.accept
+
+    expect(page).to have_content('Cliente excluído com sucesso!')
+  end
 end
