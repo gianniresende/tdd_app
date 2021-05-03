6adb21e40633e1052726d0826579e26089dd7e0e
gianniresende <gianniresende@gmail.com>
2021-05-03 14:54:57 -0300

    test: check link show in page index

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -89,4 +89,19 @@ feature "Customers", type: :feature do
     expect(page).to have_content('Cliente atualizado com sucesso!')
     expect(page).to have_content(new_name)
   end
+
+  scenario 'Click link to show' do  
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S','N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, "/html/body/table/tbody/tr/td[4]/a").click
+
+    expect(page).to have_content('Mostrando Cliente')
+  end
 end
